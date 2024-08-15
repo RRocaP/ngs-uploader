6beaf9388530d969bed5d670942b9c6bf25369a7
Ubuntu <Ramon.rocap@gmail.com>
2024-08-15 03:28:08 +0000

    Latest 1pm

--- vue.config.js
+++ vue.config.js
@@ -7,5 +7,8 @@ module.exports = defineConfig({
     vuetify: {
       // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
     }
-  }
-})
+  },
+
+  publicPath: process.env.NODE_ENV === 'production' ? '/ngs-uploader/' : '/',
+  outputDir: 'dist',
+});
